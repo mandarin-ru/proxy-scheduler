3b14c6d3833400cafa67850496ce676b4dd41e41
Serg Buglakov <sbuglakov@xiag.ch>
2021-02-07 14:08:53 +0700

    extract templates out of php to mustache; small fixes also

--- pac.php
+++ pac.php
@@ -1,4 +1,6 @@
-<?
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
 
 header("Content-type: application/x-ns-proxy-autoconfig");
 $file_name = "configurate.csv";
@@ -6,16 +8,13 @@ $file_name = "configurate.csv";
 if (($handle = fopen($file_name, "a+")) !== false) {
     while (($data = fgetcsv($handle, 0, ",")) !== false) {
         if ($data[0] == $_SERVER['REMOTE_ADDR'] && date() < $data[3]) {
-            $arParams = $data;
+            $arParams = ['ip' => $data[0], 'proxy' => $data[1], 'port' => $data[2], 'duration' => $data[3]]; ;
         }
     }
+	fclose($handle);
 }
 
-?>
-function FindProxyForURL(url, host) {
-<? if (!empty($arParams)) { ?>
-    return "PROXY <?= $arParams[1] ?>:<?= $arParams[2] ?>";
-<? } else { ?>
-    return "DIRECT";
-<? } ?>
-}
+$m = new Mustache_Engine([
+    'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/templates'),
+]);
+echo $m->render('pacfile', $arParams);
