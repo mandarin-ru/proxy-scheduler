5fc5eaad1b6f8b4c417d68d325641bf8ba09acf5
Serg Buglakov <sbuglakov@xiag.ch>
2021-02-17 00:30:44 +0700

    naive storage implementation

--- pac.php
+++ pac.php
@@ -1,20 +1,12 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+require 'Storage.php';
 
 header("Content-type: application/x-ns-proxy-autoconfig");
-$file_name = "configurate.csv";
-
-if (($handle = fopen($file_name, "a+")) !== false) {
-    while (($data = fgetcsv($handle, 0, ",")) !== false) {
-        if ($data[0] == $_SERVER['REMOTE_ADDR'] && time() < $data[3]) {
-            $arParams = ['ip' => $data[0], 'proxy' => $data[1], 'port' => $data[2], 'duration' => $data[3], 'pattern' => $data[4]]; ;
-        }
-    }
-	fclose($handle);
-}
+$storage = new Storage;
 
 $m = new Mustache_Engine([
     'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/templates'),
 ]);
-echo $m->render('pacfile', $arParams);
+echo $m->render('pacfile', ['proxy' => $storage->readRules($_SERVER['REMOTE_ADDR'])]);
